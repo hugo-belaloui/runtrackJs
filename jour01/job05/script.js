3d161f28b90deab4873de2ab5671beff163bda60
hugo-belaloui <hugo.belaloui@laplateforme.io>
2026-04-21 13:50:39 +0200

    day 01 / job 05

---
+++ jour01/job05/script.js
@@ -0,0 +1,10 @@
+function afficherJourSemaines()
+{
+    let joursSemaines = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+
+    for(let i = 0; i < joursSemaines.length; i++)
+    {
+        console.log(joursSemaines[i]);
+    }
+}
+afficherJourSemaines();
